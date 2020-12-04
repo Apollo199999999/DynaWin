a49963fa4909c11e5b7e4906eff91bd59c46d7a9
Apollo199999999 <matthiaswang60@gmail.com>
2020-12-04 21:51:38 +0800

    Add Dynamic Wallpaper Task UI complete
    Add Dynamic Wallpaper Task UI is now (mostly) complete.

--- DynaWin/AddDynamicWallpaperTask.xaml.cs
+++ DynaWin/AddDynamicWallpaperTask.xaml.cs
@@ -80,5 +80,52 @@ namespace DynaWin
         {
             //this event handler is for adding a battery event
         }
+
+        private void ActionsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //check if an item is selected in the actionslistbox
+            if (ActionsListBox.SelectedItem != null)
+            {
+                //an item is selected
+
+                /*check if the items count of the listbox 
+                 * disable the remove action button if its less than 1. Also change the tooltip text*/
+
+                if (ActionsListBox.Items.Count > 1)
+                {
+                    /*it is not the first item, change the tooltip text to "Remove an Event" and 
+                    enable the remove action button*/
+                    RemoveActionBtnToolTip.Content = "Remove an Event";
+                    RemoveActionBtn.IsEnabled = true;
+
+                }
+                else if (ActionsListBox.Items.Count < 1)
+                {
+                    /*the listbox items count is 1. Disable the remove action button and 
+                    change the tooltip text to "You cannot remove the first event"*/
+                    RemoveActionBtnToolTip.Content = "You cannot remove the first event";
+                    RemoveActionBtn.IsEnabled = false;
+
+                }
+
+            }
+            else
+            {
+                //disable the remove action button and with the tooltip text as "No event selected to remove"
+                RemoveActionBtnToolTip.Content = "There is no selected event to remove";
+                RemoveActionBtn.IsEnabled = false;
+            }
+        }
+
+        private void ActionsListBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            //this is the event handler for when the actions list box loads. Add events here by default
+        }
+
+        private void RemoveActionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //remove the selected item in the actionslistbox
+            ActionsListBox.Items.Remove(ActionsListBox.SelectedItem);
+        }
     }
 }
